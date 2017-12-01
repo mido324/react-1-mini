bc39cfc0ec509c8e271002ccd6e0afb6a973e21d
mido <mido.ghanim@gmail.com>
2017-12-01 12:12:11 -0700

    stage 4

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+  constructor() {
+    super()
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+    }
+  }
   render() {
     
      
      
     return (
       <div>
-        
+      <span>Picture:</span>
+Picture :
+       <input />
+       Name:
+        <input />
+
+        <button className="buttom">Add a friend</button>
       </div>
     );
   }
